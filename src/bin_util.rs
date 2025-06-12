8898cfcad852d004f985112d10fbdbaa6f6ba33e
sesankm <sesank.mallikarjuna@gmail.com>
2025-06-12 08:15:25 -0500

    use slices instead of vec copy

--- src/bin_util.rs
+++ src/bin_util.rs
@@ -7,20 +7,20 @@ pub fn str_to_bin(input: String) -> String {
 	output + "1"
 }
 
-pub fn bin_rot(inp: Vec<char>, bits_to_rotate: usize) -> Vec<char> {
+pub fn bin_rot(inp: &[char], bits_to_rotate: usize) -> Vec<char> {
 	let mut res = inp.to_vec();
 	res.rotate_right(bits_to_rotate);
 	res
 }
 
-pub fn bin_xor(inp1: Vec<char>, inp2: Vec<char>) -> Vec<char> {
+pub fn bin_xor(inp1: &[char], inp2: &[char]) -> Vec<char> {
 	inp1.iter()
 		.zip(inp2.iter())
 		.map(|(i_1, i_2)| ((i_1 != i_2) as i32).to_string().pop().unwrap())
 		.collect()
 }
 
-pub fn bin_and(inp1: Vec<char>, inp2: Vec<char>) -> Vec<char> {
+pub fn bin_and(inp1: &[char], inp2: &[char]) -> Vec<char> {
 	inp1.iter()
 		.zip(inp2.iter())
 		.map(|(i_1, i_2)| ((*i_1 == '1' && i_1 == i_2) as i32).to_string().pop().unwrap())
@@ -28,7 +28,7 @@ pub fn bin_and(inp1: Vec<char>, inp2: Vec<char>) -> Vec<char> {
 }
 
 
-pub fn bin_add(inp1: Vec<char>, inp2: Vec<char>) -> Vec<char> {
+pub fn bin_add(inp1: &[char], inp2: &[char]) -> Vec<char> {
 	let inp1_int = bin_to_decimal(inp1);
 	let inp2_int = bin_to_decimal(inp2);
 
@@ -40,13 +40,13 @@ pub fn bin_add(inp1: Vec<char>, inp2: Vec<char>) -> Vec<char> {
 	sum_bin.chars().collect()
 }
 
-pub fn bin_not(inp: Vec<char>) -> Vec<char> {
+pub fn bin_not(inp: &[char]) -> Vec<char> {
 	inp.into_iter()
 		.map(|i| (!(i.to_string().parse::<i32>().unwrap() == 1) as i32).to_string().pop().unwrap())
 		.collect()
 }
 
-pub fn bin_to_decimal(inp: Vec<char>) -> i128 {
+pub fn bin_to_decimal(inp: &[char]) -> i128 {
 	let inp:String = inp.into_iter().collect();
 	let inp_as_str = inp.trim();
 	if inp_as_str.is_empty() {
